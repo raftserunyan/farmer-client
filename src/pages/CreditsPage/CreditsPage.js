321b10d2723724dc10f36844ef5476008197c8a2
rafik.tserunyan <rafiktserunyan@gmail.com>
2024-02-27 13:54:06 +0400

    My Credits/My Debths added

--- src/pages/CreditsPage/CreditsPage.js
+++ src/pages/CreditsPage/CreditsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Table } from 'components';
 import * as S from './CreditsPage.styles';
@@ -6,17 +6,45 @@ import { Layout } from 'components/Layout';
 import { tableColumns } from 'constants/tableColumns';
 
 export const CreditsPage = ({ credits, loadCredits }) => {
-	return (
-		<Layout>
-			<S.CreditsPageContainer>
-				<Table
-					title='Պարտքեր'
-					data={credits.list}
-					total={credits.length}
-					loadData={loadCredits}
-					columns={tableColumns.credits}
-				/>
-			</S.CreditsPageContainer>
-		</Layout>
-	);
+  const [selectedOption, setSelectedOption] = useState("myDebths");
+  
+  const myDebthsClicked = () => {
+		loadCredits('', {});
+		setSelectedOption('myDebths');
+	};
+
+	const myCreditsClicked = () => {
+		loadCredits('myCredits=true', {});
+		setSelectedOption('myCredits');
+	};
+
+  return (
+    <Layout>
+      <S.CreditsPageContainer>
+        <S.FlexColumn>
+          <S.RadioInputs>
+			<S.CustomLabel>
+				<S.CustomRadio id="myDebths" name="creditType" value="myDebths" style={{ borderRadius: "1em 0 0 1em" }}
+					checked={selectedOption === 'myDebths'} onChange={myDebthsClicked} />
+				<S.CustomSpan>Ինձ են պարտք</S.CustomSpan>
+			</S.CustomLabel>
+			
+			<S.CustomLabel>
+				<S.CustomRadio id="myCredits" name="creditType" value="myCredits" style={{ borderRadius: "0 1em 1em 0" }}
+					checked={selectedOption === 'myCredits'} onChange={myCreditsClicked} />
+				<S.CustomSpan>Ես եմ պարտք</S.CustomSpan>				
+			</S.CustomLabel>
+          </S.RadioInputs>
+              
+          <Table
+            title='Պարտքեր'
+            data={credits.list}
+            total={credits.length}
+            loadData={() => loadCredits('', {})}
+            columns={tableColumns.credits}
+          />
+        </S.FlexColumn>
+      </S.CreditsPageContainer>
+    </Layout>
+  );
 };
--- src/pages/CreditsPage/CreditsPage.styles.js
+++ src/pages/CreditsPage/CreditsPage.styles.js
@@ -1,8 +1,63 @@
 import styled from 'styled-components';
 
+export const CustomSpan = styled.span`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+export const CustomRadio = styled.input.attrs({ type: 'radio' })`
+    margin: 0;
+    appearance: none;
+    width: 20em;
+    height: 3em;
+    background-color: rgb(217, 217, 217);
+    outline: none;
+    transition: 0.1s all linear;
+    vertical-align: middle;
+    box-shadow: inset -1px -1px 3px rgb(90, 90, 90), inset -3px -3px 5px rgb(163, 163, 163), inset 2px 2px 3px rgb(255, 255, 255), inset 4px 4px 5px rgb(225, 225, 225);
+
+    &:checked {
+        background: rgba(54, 145, 140, 1);
+        box-shadow: inset -1px -1px 3px rgb(147, 220, 216), inset -3px -3px 5px rgb(125, 200, 195), inset 1px 1px 3px rgb(14, 49, 47), inset 2px 2px 4px rgb(55, 97, 94);
+    }
+`;
+
+export const CustomLabel = styled.label`
+    position: relative;
+    cursor: pointer;
+    display: inline-block;
+	font-size: 18px;
+  	font-weight: 500;
+	color: #212121;
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+`;
+
+export const Center = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+export const FlexColumn = styled.div`
+    display: flex; 
+    flex-direction: column;
+`;
+
+export const FlexRow = styled.div`
+    display: flex; 
+    flex-direction: row;
+`;
+
 export const CreditsPageContainer = styled.div`
-	width: calc(100% - 60px);
-	padding: 30px;
-	display: flex;
-	gap: 30px;
+    width: calc(100% - 60px);
+    padding: 30px;
+    display: flex;
+    gap: 30px;
+`;
+
+export const RadioInputs = styled.div`
+	margin: 0 0 30px 0;
 `;
